b2802bf80cbb4740745f0b9c179dff2fc7bd22c0
Dhruv Mishra <115948716+DHRUV4703@users.noreply.github.com>
2024-06-29 21:30:01 +0530

    Add files via upload

---
+++ Data Cleaning Project.sql
@@ -0,0 +1,12 @@
+/*
+Cleaning Data in SQL Queries
+*/
+
+SELECT*
+FROM [Portfolio Project].dbo.NashvilleHousing
+
+-----------------------------------------------------------------------
+
+--Standardize Date Format
+
+
